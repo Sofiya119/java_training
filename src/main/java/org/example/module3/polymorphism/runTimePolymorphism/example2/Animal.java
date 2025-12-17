6fa2ec1f047295ee5b2a853c9f5a92feb5f9d59e
sofiya <sofiyakhan1109@gmail.com>
2025-12-17 16:08:35 +0530

    a

---
+++ src/main/java/org/example/module3/polymorphism/runTimePolymorphism/example2/Animal.java
@@ -0,0 +1,7 @@
+package org.example.module3.polymorphism.runTimePolymorphism.example2;
+
+public class Animal {
+    public void makeSound() {
+        System.out.println("default sound");
+    }
+}
